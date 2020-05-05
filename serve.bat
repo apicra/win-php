379cec695ebd1626667d3397a3a4b6d806f0f182
tom <tom@sapletta.com>
2020-05-05 15:15:45 +0200

    update php examples

--- serve.bat
+++ serve.bat
@@ -1,3 +1,6 @@
-php-7.3.6-nts-Win32-VC15-x86\php.exe -S %~1
+@echo off
+set PHP_LOG=php-log.txt
+start php-7.3.6-nts-Win32-VC15-x86\php.exe -S %~1 /qn /L*v %PHP_LOG%
+::exit
 ::exec("d:/path/to/php.exe d:/wamp/www/diplomski/program/defender/tester.php", $output);
 ::print_r($output);
