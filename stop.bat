15282c8c15fe9441fa0d48074efee4f9380d2c8c
tom <tom@sapletta.com>
2020-05-05 15:52:38 +0200

    change port from 80 to 8080

--- stop.bat
+++ stop.bat
@@ -1,9 +1,8 @@
 @ECHO OFF
 netstat -a -n -o | findstr /i ":8080"
 set "lastoccur="
-FOR /F "tokens=5" %%T IN ('netstat -a -n -o ^| findstr ":8080" ') DO set "lastoccur=%%T"
-set lastoccur
-echo "%lastoccur%"
-echo ProcessId to kill = %lastoccur%
-taskkill /pid %lastoccur% /F /T
-wmic process where "ParentProcessId=%lastoccur%" delete
+FOR /F "tokens=5" %%T IN ('netstat -a -n -o ^| findstr ":8080" ') DO (
+echo ProcessId to kill = %%T
+taskkill /pid %%T /F /T
+wmic process where "ParentProcessId=%%T" delete
+exit)
