cf574ccdb651cb3d580f98e8b00717c28ca0af9f
tom <tom@sapletta.com>
2020-05-05 15:40:38 +0200

    change port from 80 to 8080

--- status.bat
+++ status.bat
@@ -1,4 +1,4 @@
 where php
-netstat -ano|findstr "PID :80"
-netstat -bano|findstr "PID :80"
-netstat -anb -p tcp|findstr "PID :80"
+netstat -ano|findstr "PID :8080"
+netstat -bano|findstr "PID :8080"
+netstat -anb -p tcp|findstr "PID :8080"
