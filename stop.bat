c874273c703791e535147847a683425daa4c6f86
tom <tom@sapletta.com>
2020-05-05 16:29:40 +0200

    change port from 80 to 8080

--- stop.bat
+++ stop.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 tasklist /v | find "php.exe"
-FOR /F "tokens=5" %%T IN ('tasklist /v | find "php.exe" ') DO (
-echo ProcessId to kill = %%T
-taskkill /pid %%T /F /T
-wmic process where "ParentProcessId=%%T" delete)
+taskkill /F /IM php.exe
+::FOR /F "tokens=5" %%T IN ('tasklist /v | find "php.exe" ') DO (
+::echo ProcessId to kill = %%T
+::taskkill /pid %%T /F /T
+::wmic process where "ParentProcessId=%%T" delete)
