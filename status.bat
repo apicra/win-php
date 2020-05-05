7bc08d6a473d89140659fe36887a44c75d86ec29
tom <tom@sapletta.com>
2020-05-05 16:02:42 +0200

    change port from 80 to 8080

--- status.bat
+++ status.bat
@@ -1,4 +1,6 @@
+@echo off
 where php
 netstat -ano|findstr "PID :8080"
-netstat -bano|findstr "PID :8080"
-netstat -anb -p tcp|findstr "PID :8080"
+::netstat -bano|findstr "PID :8080"
+::netstat -anb -p tcp|findstr "PID :8080"
+tasklist /v | find "php.exe"
--- stop.bat
+++ stop.bat
@@ -1,8 +1,6 @@
 @ECHO OFF
-netstat -a -n -o | findstr /i ":8080"
-set "lastoccur="
-FOR /F "tokens=5" %%T IN ('netstat -a -n -o ^| findstr ":8080" ') DO (
+tasklist /v | find "php.exe"
+FOR /F "tokens=5" %%T IN ('tasklist /v | find "php.exe" ') DO (
 echo ProcessId to kill = %%T
 taskkill /pid %%T /F /T
-wmic process where "ParentProcessId=%%T" delete
-exit)
+wmic process where "ParentProcessId=%%T" delete)
